baa52622bd7d26209d53aff4059b181187764715
ArthurFelipePallu <arthurfelipepallu@gmail.com>
2025-12-07 17:57:10 -0300

    Movement Exceptions

--- Chess/Exceptions/ChessException.cs
+++ Chess/Exceptions/ChessException.cs
@@ -1,6 +1,11 @@
-﻿namespace Chess_Console_Project.Board.Exceptions;
+﻿namespace Chess_Console_Project.Chess.Exceptions;
 
 public class ChessException(string msg): Exception(msg)
+{
+    
+}
+
+public class MovementException(string msg) : ChessException(msg)
 {
     
 }
